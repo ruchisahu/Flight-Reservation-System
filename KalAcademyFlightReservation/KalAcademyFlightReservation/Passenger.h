aed0676a480d9fa2f3533f00866946fe64386cd9
tigeringarden <steventranucsd@outlook.com>
2018-07-12 12:23:36 -0700

    change models to accommodate feature changes

--- KalAcademyFlightReservation/KalAcademyFlightReservation/Passenger.h
+++ KalAcademyFlightReservation/KalAcademyFlightReservation/Passenger.h
@@ -5,43 +5,6 @@ namespace KalAcademyFlightReservation
 {
 	class Passenger
 	{
-	public:
-		Passenger() = default;
-		Passenger(const unsigned int id, const std::string& lastName, const std::string& firstName, const std::string& dateofBirth, const std::string& gender, const std::string& address, const unsigned int phone, const unsigned int ccNumber);
-		~Passenger();
 
-		void setId(const int id);
-		const int getId() const;
-
-		void setLastName(const std::string& lastName);
-		const std::string& getLastName() const;
-
-		void setFirstName(const std::string& firstName);
-		const std::string& getFirstName() const;
-
-		void setDateofBirth(const std::string& dateofBirth);
-		const std::string& getDateofBirth() const;
-
-		void setGender(const std::string& gender);
-		const std::string& getGender() const;
-
-		void setAddress(const std::string& address);
-		const std::string& getAddress() const;
-
-		void setPhone(const unsigned int phone);
-		const unsigned int getPhone() const;
-
-		void setCCNumber(const unsigned int ccNumber);
-		const unsigned int getCCNumber() const;
-
-	private:
-		unsigned int id;
-		std::string lastName;
-		std::string firstName;
-		std::string dateOfBirth;
-		std::string gender;
-		std::string address;
-		unsigned int phone;
-		unsigned int ccNumber;
 	};
 }
--- KalAcademyFlightReservation/KalAcademyFlightReservation/Ticket.cpp
+++ KalAcademyFlightReservation/KalAcademyFlightReservation/Ticket.cpp
@@ -6,44 +6,5 @@
 
 namespace KalAcademyFlightReservation
 {
-	Ticket::Ticket(const unsigned int id, const Passenger& passenger, const Flight& flight)
-	{
-		this->id = id;
-		this->passenger = passenger;
-		this->flight = flight;
-	}
 
-	Ticket::~Ticket()
-	{
-	}
-
-	void Ticket::setId(const int id)
-	{
-		this->id = id;
-	}
-
-	const int Ticket::getId() const
-	{
-		return id;
-	}
-
-	void Ticket::setPassenger(const Passenger& passenger)
-	{
-		this->passenger = passenger;
-	}
-
-	const Passenger & Ticket::getPassenger() const
-	{
-		return passenger;
-	}
-
-	void Ticket::setFlight(const Flight& flight)
-	{
-		this->flight = flight;
-	}
-
-	const Flight & Ticket::getFlight() const
-	{
-		return flight;
-	}
 }
