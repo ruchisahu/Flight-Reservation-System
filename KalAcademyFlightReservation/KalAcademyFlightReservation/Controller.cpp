bca7619674bb4268d2b41219803b48970eea4a18
Madhura Pattekar <madhuracp@hotmail.com>
2018-07-12 22:06:17 -0700

    Adding Controller API's so that UI work can be unblocked. It does not integrate with dataccess yet.

---
+++ KalAcademyFlightReservation/KalAcademyFlightReservation/Controller.cpp
@@ -0,0 +1,47 @@
+#include "stdafx.h"
+#include <string>
+#include <vector>
+#include "Passenger.h"
+#include"Ticket.h"
+#include "Flight.h"
+#include "DataAccess.h"
+#include "Controller.h"
+
+namespace KalAcademyFlightReservation
+{
+    // Default constructor - creates an instance of DataAccess class.
+    Controller::Controller()
+    {
+        dataAccess = new DataAccess();
+    }
+
+    // Destructor
+    Controller::~Controller()
+    {
+        // Free the memory that was allocated in the constructor
+        delete dataAccess;
+    }
+
+    // Returns list of one-way Flight objects that match the specified search criteria {origin, destination and date}.
+    std::vector<Flight> Controller::GetFlightSchedule(std::string origin, std::string destination, tm date)
+    {
+    }
+
+    // Returns Flight for the specified flight number.
+    // Returns nullptr if invalid flight number is specified.
+    Flight* Controller::GetFlightInformation(int flightNumber)
+    {
+    }
+
+    // Returns Passenger for the specified PassportId which is unique identifier for a passenger.
+    // Returns nullptr if passenger corresponding to specified passportId is not found.
+    Passenger* Controller::GetPassengerInformation(int passportId)
+    {
+    }
+
+    // Returns Ticket for the specified ticket number.
+    // Returns nullptr if ticket corresponding to the specified ticket number is not found.
+    Ticket* Controller::GetTicketInformation(int ticketNo)
+    {
+    }
+}
--- KalAcademyFlightReservation/KalAcademyFlightReservation/Controller.h
+++ KalAcademyFlightReservation/KalAcademyFlightReservation/Controller.h
@@ -1,21 +1,39 @@
 #pragma once
 #include <string>
-#include "Passenger.h";
-#include"Ticket.h";
-#include "Flight.h";
+#include "Passenger.h"
+#include"Ticket.h"
+#include "Flight.h"
 #include <ctime>
 #include <vector>
+#include "DataAccess.h"
 
-class Controller
+namespace KalAcademyFlightReservation
 {
-public:
-    //Getters
-    
-    Flight GetFlightInformation(int flightNumber);
+    class Controller
+    {
+    public:
+        Controller();
+        ~Controller();
 
-    std::vector<Flight> GetFlightSchedule(std::string origin,std::string destination,tm date);
+        //Getters
 
-    Passenger GetPassengerInformation(int passPortId);
+        // Returns list of one-way Flight objects that match the specified search criteria {origin, destination and date}.
+        std::vector<Flight> GetFlightSchedule(std::string origin, std::string destination, tm date);
 
-    Ticket GetTicketInformation(int ticketNo);
-};
+        // Returns Flight for the specified flight number.
+        // Returns nullptr if invalid flight number is specified.
+        Flight* GetFlightInformation(int flightNumber);
+
+        // Returns Passenger for the specified PassportId which is unique identifier for a passenger.
+        // Returns nullptr if passenger corresponding to specified passportId is not found.
+        Passenger* GetPassengerInformation(int passportId);
+
+        // Returns Ticket for the specified ticket number.
+        // Returns nullptr if ticket corresponding to the specified ticket number is not found.
+        Ticket* GetTicketInformation(int ticketNo);
+
+    private:
+        // DataAccess class member variable
+        DataAccess *dataAccess;
+    };
+}
